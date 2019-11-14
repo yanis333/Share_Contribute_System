886018ed365bbcc6878ea287be67343646556648
Skander <skander96@hotmail.com>
2019-11-14 01:41:24 -0500

    can Search for groups , Open group, and see list of groups for user #4

---
+++ Controller/GroupController/getMyGroups.php
@@ -0,0 +1,28 @@
+<?php
+    include('../../Model/Config/db_server.php');
+    session_start();
+    $db = new DB();
+    $arrayInfo = array();
+    $allInfo= array();
+    $arrayInfo[0] = false;
+    if(isset($_SESSION['username'])){ 
+    if($_SESSION["username"] != null){
+
+
+
+        $result = $db->query("select name from groups where managerID=".$_SESSION['usernameId']." ");
+        
+        $allInfo = array();
+
+        if($result){
+            while($row = $result->fetch_assoc()){
+                $allInfo[] = $row;
+            }
+            $arrayInfo[0] = true;
+            $arrayInfo[1] = $allInfo;
+        }
+
+    }
+}
+echo json_encode($arrayInfo);
+?>
--- Controller/GroupController/searchGroup.php
+++ Controller/GroupController/searchGroup.php
@@ -1,14 +1,14 @@
 <?php
-     include('../../Model/Config/db_server.php');
-     session_start();
-     $db = new DB();
-     $arrayInfo = array();
-     $arrayInfo[0] = false;
-     if(isset($_SESSION['username'])){
+    include('../../Model/Config/db_server.php');
+    session_start();
+    $db = new DB();
+    $arrayInfo = array();
+    $arrayInfo[0] = false;
 
-        if($_SESSION["username"]!=null && $_SESSION['isAdmin'] == 1){
+    if($_SESSION["username"]!=null && $_SESSION['isAdmin'] == 1){
 
-            $result = $db->query("select g.ID,g.name,e.name as eventName from groups as g left join events as e on e.ID=g.eventID");
+            $name = $_POST['name'];
+            $result = $db->query("select g.ID,g.name,e.name as eventName from groups as g left join events as e on e.ID=g.eventID where g.name like '%".$name."%' order by g.name Asc");
                 
             $allInfo = array();
     
@@ -29,7 +29,7 @@
                                     Case When true then 1 end as isRegistered
                                     from groups as g 
                                     left join events as e on e.ID=g.eventID
-                                    where g.id in (select gp.groupID from groupparticipants as gp where gp.userID =".$_SESSION['usernameId'].")");
+                                    where g.id in (select gp.groupID from groupparticipants as gp where gp.userID =".$_SESSION['usernameId']." and g.name like '%".$name."%' order by g.name Asc)");
             $allInfo = array();
 
             if($result){
@@ -47,10 +47,10 @@
                                      (select e.Id 
                                         from eventparticipants as ep 
                                         inner join events as e on e.Id = ep.eventID 
-                                        where ep.userid=".$_SESSION['usernameId'].")  AND NOT g.ID in 
+                                        where ep.userid=1)  AND NOT g.ID in 
                                         (select gp2.groupID 
                                             from groupparticipants as gp2 
-                                                   where gp2.userID)");
+                                                   where gp2.userID) AND  g.name like '%$.$name.%' order by g.name Asc");
 
             if($result){
                         while($row = $result->fetch_assoc()){
@@ -61,12 +61,6 @@
                     }
 
 
-                
-
-        }
-
-     }
-
-  
+    }
     echo json_encode($arrayInfo);
 ?>
